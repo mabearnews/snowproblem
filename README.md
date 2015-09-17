9d3df44be3d8d7e6f2b27e6eb6c44ea7531a68e9
Jack Beasley <jack.beasley@gmail.com>
2015-09-17 15:19:57 -0700

    Fix build system

--- README.md
+++ README.md
@@ -3,19 +3,11 @@
 
 Initial Wordpress theme for M-A Bear News.
 
-## SASS
-Compile with 'compass watch'
-
-## Coffee
-Compile with 'coffee --watch --output js --compile coffee'
-
-## All
-Compile the __Entire__ Sass and Coffee with the command `./scripts/watch.sh`
+## Compile instructions
+- Install ruby gem: 'gem install compass'
+- Install node dependencies: 'npm install'
+- Build and zip theme: 'gulp'
 
 ## Install
-To install the theme follow the following commands.
+Use the zip file in dist to install
 
-cd scripts
-./make_production.sh
-
-Do __NOT__ zip the entire folder as you will include nonessential files. Especially do not if you are on a computer which has the sass cache and the .git folders which will add another 8mb to the file size.
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var compass = require('gulp-compass');
@@ -9,24 +9,31 @@ var zip = require('gulp-zip');
 gulp.task('compass', function() {
   gulp.src('./sass/*.scss')
     .pipe(compass({
-      config_file: './config.rb',
+    config_file: './config.rb',
       css: 'stylesheets',
       sass: 'sass'
     }))
-    //.pipe(gulp.dest('app/assets/temp'));
-});
+  //.pipe(gulp.dest('app/assets/temp'));
+  });
 
-gulp.task('coffee', function() {
+  gulp.task('coffee', function() {
   gulp.src('./coffee/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./js/'))
-});
+  });
 
-gulp.task('zip', ["compass", "coffee"], function() {
-  gulp.src('.')
+  gulp.task('zip', ["compass", "coffee"], function() {
+  gulp.src(['./*',
+    '!./coffee',
+    '!./sass',
+    '!./scripts',
+    '!./.sass-cache',
+    '!./.git',
+    '!./dist'
+  ])
     .pipe(zip('snowproblem.zip'))
     .pipe(gulp.dest('dist'));
 
-});
+  });
 
-gulp.task('default', ['zip']);
+  gulp.task('default', ['zip']);
